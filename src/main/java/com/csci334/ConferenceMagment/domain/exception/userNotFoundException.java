78371532c9bb95cac12dae26abe15bbd63de189f
yc325 <yc325@uowmail.edu.au>
2023-03-19 13:57:42 +1100

    Added React.js Project

--- src/main/java/com/csci334/ConferenceMagment/domain/exception/userNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.csci334.ConferenceMagment.domain.exception;
-
-import java.text.MessageFormat;
-
-public class userNotFoundException extends RuntimeException {
-    public userNotFoundException(String username) {
-        super(MessageFormat.format("user not found by username{0}",username));
-    }
-}
--- src/main/java/com/csci334/ConferenceMagment/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csci334.ConferenceMagment.repository;
-
-import com.csci334.ConferenceMagment.domain.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, String> {
-}
